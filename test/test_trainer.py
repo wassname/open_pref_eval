180ec46869c71d9a46f06b7addcf2a601b9d23f3
wassname <1103714+wassname@users.noreply.github.com>
2025-06-01 15:05:02 +0800

    wip

--- test/test_trainer.py
+++ test/test_trainer.py
@@ -2,81 +2,58 @@ import tempfile
 import unittest
 
 import pytest
-import torch
-from parameterized import parameterized
-from datasets import Dataset, features
-from transformers import (
-    AutoModelForCausalLM,
-    AutoModelForSeq2SeqLM,
-    AutoModelForVision2Seq,
-    AutoProcessor,
-    AutoTokenizer,
-)
+# from parameterized import parameterized
 from datasets import load_dataset
 
 
-from open_pref_eval.trainer import get_dummy_trainer, dummy_dataset, OPEConfig, OPETrainer
-from open_pref_eval.evaluation import evaluate, eval_dataset, evaluate_model
+# from open_pref_eval.trainer import dummy_dataset, OPEConfig, OPETrainer
+from open_pref_eval.evaluation import evaluate, evaluate_model
 from open_pref_eval.helpers.load_models import load_peft_model
 
 MODELS = [
             # https://huggingface.co/models?other=base_model%3Aadapter%3Aunsloth%2Ftinyllama
-            ["pacozaa/tinyllama-alpaca-lora"],
+            "pacozaa/tinyllama-alpaca-lora", # 101mb
+            "snake7gun/tiny-random-qwen3",
+            "HuggingFaceTB/SmolLM2-135M-Instruct",
 
-            ["gepardzik/LLama-3-8b-rogue-lora"],
-            ["bunnycore/Phi-3.5-mini-lora-rp"],
+            # ["gepardzik/LLama-3-8b-rogue-lora"],
+            "bunnycore/Phi-3.5-mini-lora-rp",
             # ["tloen/alpaca-lora-7b"],
             # ["t5"], # TODO make it work for encoder_decoder
 ]
 
-class DPOTrainerTester(unittest.TestCase):
+N = 80
+imdb = load_dataset('wassname/imdb_preferences', split=f'test[:{N}]', keep_in_memory=False)
+datasets = [imdb]
 
-    def setUp(self):
-        N = 80
-        imdb = load_dataset('wassname/imdb_preferences', split=f'test[:{N}]', keep_in_memory=False)
-        self.datasets = [imdb]
 
-    @parameterized.expand(
-        MODELS
+@pytest.mark.parametrize(
+    "model_name",
+    MODELS
+)
+def test_evaluate_model( model_name):
+    print('testing', model_name)
+    model, tokenizer = load_peft_model(model_name)
+
+    df, df_raw = evaluate_model(
+        model=model,
+        tokenizer=tokenizer,
+        datasets=datasets,
     )
-    def test_preferences_trainer(self, model_name):
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            print('testing', model_name)
-            model, tokenizer = load_peft_model(model_name)
 
-            training_args = OPEConfig(
-                output_dir=tmp_dir,
-                per_device_eval_batch_size=2,
-            )
-
-            trainer = OPETrainer(
-                model=model,
-                tokenizer=tokenizer,
-                args=training_args,
-                train_dataset=dummy_dataset,
-                eval_dataset=dummy_dataset,
-            )
-
-            df, df_raw = evaluate_model(
-                trainer=trainer, 
-                datasets=self.datasets,
-            )
-
-            print(df)
-            assert df['correct'].iloc[0]>0.5
+    print('df_raw', df_raw.groupby(["model", "dataset"], dropna=False).mean())
+    print(df)
+    assert df['correct'].iloc[0]>0.5
 
 
-    @parameterized.expand(
-        MODELS[:1],
-    )
-    def test_evaluate(self, model_name):
-        df = evaluate(
-            model_names=[model_name], datasets=[dummy_dataset],
-            batch_size=4,
-            )
-        print(df)
-        assert df['correct'].iloc[0]>0.5
-
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize(
+    "model_name",
+    MODELS[:1],
+)
+def test_evaluate(model_name):
+    df_agg, _ = evaluate(
+        model_names=[model_name], datasets=datasets,
+        batch_size=4,
+        )
+    print(df_agg)
+    assert df_agg['correct'].iloc[0]>0.5
